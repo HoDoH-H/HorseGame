b49145b036ac6f31b62f8e9d4c817198fd44468d
HoDoH-H <144995071+HoDoH-H@users.noreply.github.com>
2025-01-14 12:02:18 +0100

    finished base layout

--- main.py
+++ main.py
@@ -6,6 +6,20 @@ from tkinter import *
 
 root = tk.Tk()
 
+#region Colors
+bgColor = "gray20"
+bgColorBis = "gray35"
+
+redColor = "red"
+redLocationColors = "red4"
+greenColor = "green"
+greenLocationColors = "green4"
+yellowColor = "yellow"
+yellowLocationColors = "goldenrod"
+blueColor = "blue"
+blueLocationColors = "blue4"
+#endregion Colors
+
 #region Main Page Setter
 
 size = 900
@@ -25,28 +39,55 @@ def DrawBoard():
     Used to draw the board on the root windows inside a canvas
     """
     #region Draw players camps
-    canvas.create_rectangle(0, 0, locationSize*6 - borderSize, locationSize*6 - borderSize, fill="red", outline="gray35")
-    canvas.create_rectangle(0, size, locationSize*6 - borderSize, size + borderSize - locationSize*6, fill="blue", outline="gray35")
-    canvas.create_rectangle(size + borderSize - locationSize*6, 0, size, locationSize*6 - borderSize, fill="green", outline="gray35")
-    canvas.create_rectangle(size + borderSize - locationSize*6, size + borderSize - locationSize*6, size, size, fill="yellow", outline="gray35")
+    canvas.create_rectangle(0, 0, locationSize*6 - borderSize, locationSize*6 - borderSize, fill=redLocationColors, outline=bgColorBis)
+    canvas.create_rectangle(0, size, locationSize*6 - borderSize, size + borderSize - locationSize*6, fill=blueLocationColors, outline=bgColorBis)
+    canvas.create_rectangle(size + borderSize - locationSize*6, 0, size, locationSize*6 - borderSize, fill=greenLocationColors, outline=bgColorBis)
+    canvas.create_rectangle(size + borderSize - locationSize*6, size + borderSize - locationSize*6, size, size, fill=yellowLocationColors, outline=bgColorBis)
     #endregion Draw players camps
 
     #region Draw main locations
     for i in range(15): 
         if(i != 7):
-            canvas.create_oval(locationSize * i + borderSize, locationSize * 6 + borderSize, locationSize * i + locationDisplaySize, locationSize * 6 + locationDisplaySize, fill="gray20", outline='gray35')
-            canvas.create_oval(locationSize * i + borderSize, locationSize * 8 + borderSize, locationSize * i + locationDisplaySize, locationSize * 8 + locationDisplaySize, fill="gray20", outline='gray35')
+            if(i == 0):
+                canvas.create_oval(locationSize * i + borderSize, locationSize * 6 + borderSize, locationSize * i + locationDisplaySize, locationSize * 6 + locationDisplaySize, fill=redLocationColors, outline=bgColorBis)
+            else:
+                canvas.create_oval(locationSize * i + borderSize, locationSize * 6 + borderSize, locationSize * i + locationDisplaySize, locationSize * 6 + locationDisplaySize, fill=bgColor, outline=bgColorBis)
+            if(i == 14):
+                canvas.create_oval(locationSize * i + borderSize, locationSize * 8 + borderSize, locationSize * i + locationDisplaySize, locationSize * 8 + locationDisplaySize, fill=yellowLocationColors, outline=bgColorBis)
+            else:
+                canvas.create_oval(locationSize * i + borderSize, locationSize * 8 + borderSize, locationSize * i + locationDisplaySize, locationSize * 8 + locationDisplaySize, fill=bgColor, outline=bgColorBis)
     
     for i in range(15): 
         if(i != 7):
-            canvas.create_oval(locationSize * 6 + borderSize, locationSize * i + borderSize, locationSize * 6 + locationDisplaySize, locationSize * i + locationDisplaySize, fill="gray20", outline='gray35')
-            canvas.create_oval(locationSize * 8 + borderSize, locationSize * i + borderSize, locationSize * 8 + locationDisplaySize, locationSize * i + locationDisplaySize, fill="gray20", outline='gray35')
+            if(i == 14):
+                canvas.create_oval(locationSize * 6 + borderSize, locationSize * i + borderSize, locationSize * 6 + locationDisplaySize, locationSize * i + locationDisplaySize, fill=blueLocationColors, outline=bgColorBis)
+            else:
+                canvas.create_oval(locationSize * 6 + borderSize, locationSize * i + borderSize, locationSize * 6 + locationDisplaySize, locationSize * i + locationDisplaySize, fill=bgColor, outline=bgColorBis)
+            if(i == 0):
+                canvas.create_oval(locationSize * 8 + borderSize, locationSize * i + borderSize, locationSize * 8 + locationDisplaySize, locationSize * i + locationDisplaySize, fill=greenLocationColors, outline=bgColorBis)
+            else:
+                canvas.create_oval(locationSize * 8 + borderSize, locationSize * i + borderSize, locationSize * 8 + locationDisplaySize, locationSize * i + locationDisplaySize, fill=bgColor, outline=bgColorBis)
     
-    canvas.create_oval(locationSize * 0 + borderSize, locationSize * 7 + borderSize, locationSize * 0 + locationDisplaySize, locationSize * 7 + locationDisplaySize, fill="gray20", outline='gray35')
-    canvas.create_oval(locationSize * 7 + borderSize, locationSize * 14 + borderSize, locationSize * 7 + locationDisplaySize, locationSize * 14 + locationDisplaySize, fill="gray20", outline='gray35')
-    canvas.create_oval(locationSize * 7 + borderSize, locationSize * 0 + borderSize, locationSize * 7 + locationDisplaySize, locationSize * 0 + locationDisplaySize, fill="gray20", outline='gray35')
-    canvas.create_oval(locationSize * 14 + borderSize, locationSize * 7 + borderSize, locationSize * 14 + locationDisplaySize, locationSize * 7 + locationDisplaySize, fill="gray20", outline='gray35')
+    canvas.create_oval(locationSize * 0 + borderSize, locationSize * 7 + borderSize, locationSize * 0 + locationDisplaySize, locationSize * 7 + locationDisplaySize, fill=bgColor, outline=bgColorBis)
+    canvas.create_oval(locationSize * 7 + borderSize, locationSize * 14 + borderSize, locationSize * 7 + locationDisplaySize, locationSize * 14 + locationDisplaySize, fill=bgColor, outline=bgColorBis)
+    canvas.create_oval(locationSize * 7 + borderSize, locationSize * 0 + borderSize, locationSize * 7 + locationDisplaySize, locationSize * 0 + locationDisplaySize, fill=bgColor, outline=bgColorBis)
+    canvas.create_oval(locationSize * 14 + borderSize, locationSize * 7 + borderSize, locationSize * 14 + locationDisplaySize, locationSize * 7 + locationDisplaySize, fill=bgColor, outline=bgColorBis)
     #endregion Draw main locations
+
+    #region Draw ladders
+    for i in range(1, 7):
+        canvas.create_rectangle(locationSize * i + borderSize, locationSize * 7 + borderSize, locationSize * i + locationDisplaySize, locationSize * 7 + locationDisplaySize, fill=redLocationColors, outline=bgColorBis)
+        canvas.create_rectangle(locationSize * (i + 7) + borderSize, locationSize * 7 + borderSize, locationSize * (i + 7) + locationDisplaySize, locationSize * 7 + locationDisplaySize, fill=yellowLocationColors, outline=bgColorBis)
+
+    for i in range(1, 7):
+        canvas.create_rectangle(locationSize * 7 + borderSize, locationSize * i + borderSize, locationSize * 7 + locationDisplaySize, locationSize * i + locationDisplaySize, fill=greenLocationColors, outline=bgColorBis)
+        canvas.create_rectangle(locationSize * 7 + borderSize, locationSize * (i + 7) + borderSize, locationSize * 7 + locationDisplaySize, locationSize * (i + 7) + locationDisplaySize, fill=blueLocationColors, outline=bgColorBis)
+
+    canvas.create_polygon([locationSize * 7 + locationSize/2, locationSize * 8 - locationSize/2, locationSize * 7 + borderSize, locationSize * 8 - borderSize, locationSize * 8 - borderSize, locationSize * 8 - borderSize], fill=blueLocationColors)
+    canvas.create_polygon([locationSize * 7 + locationSize/2, locationSize * 8 - locationSize/2, locationSize * 7 + borderSize, locationSize * 7 + borderSize, locationSize * 8 - borderSize, locationSize * 7 + borderSize], fill=greenLocationColors)
+    canvas.create_polygon([locationSize * 7 + locationSize/2, locationSize * 8 - locationSize/2, locationSize * 7 + borderSize, locationSize * 8 - borderSize, locationSize * 7 + borderSize, locationSize * 7 + borderSize], fill=redLocationColors)
+    canvas.create_polygon([locationSize * 7 + locationSize/2, locationSize * 8 - locationSize/2, locationSize * 8 - borderSize, locationSize * 8 - borderSize, locationSize * 8 - borderSize, locationSize * 7 + borderSize], fill=yellowLocationColors)
+    #endregion Draw ladders
 
     # Update Canvas
     canvas.pack()
